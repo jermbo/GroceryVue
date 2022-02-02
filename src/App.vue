a89c58ec9838df9459adbc50ac98240ad7d7b1e4
jermbo <jermbo@users.noreply.github.com>
2022-02-02 17:26:15 -0500

    Updating Structure

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="main">
+    <h1>Vue + Vite + Pinia + Tailwind</h1>
+  </div>
+</template>
+
+<style>
+body {
+  @apply bg-gray-300;
+}
+.main {
+  @apply p-8;
+}
+</style>
